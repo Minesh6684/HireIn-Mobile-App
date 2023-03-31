1097d05ef0b8d1768eda0e2bcaf7acfe10e413a0
Persistent-A <apurva.mili@gmail.com>
2023-03-31 11:59:45 -0400

    Picker dropdown for search services

---
+++ components/SearchServices.js
@@ -0,0 +1,15 @@
+import { View, Picker } from "react-native";
+
+const searchServices = () => {
+    <View>
+        <Picker>
+            <Picker.Item label="House Keeping" value="" />
+            <Picker.Item label="Cook" value="" />
+            <Picker.Item label="Pet Walker" value="" />
+            <Picker.Item label="Geriatric Care" value="" />
+            <Picker.Item label="" value="" />
+        </Picker>
+    </View>
+};
+
+export default searchServices;
