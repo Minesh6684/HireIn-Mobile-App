35a1f26c6aacd191ac7272f889dd54bb53a681dc
Persistent-A <apurva.mili@gmail.com>
2023-04-29 02:30:59 -0400

    Address field string to object

--- components/EmployeeProfile.js
+++ components/EmployeeProfile.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, Image, TouchableOpacity, TextInput, KeyboardAvoidingView } from 'react-native';
+import DropDownPicker from 'react-native-dropdown-picker'
 const photo = require('../assets/images/Employer.jpg');
 import { useRecoilState } from 'recoil';
 import { userState, updateEmployee } from '../auth/authSlice';
@@ -10,7 +11,8 @@ const UserProfile = () => {
   const employeeData = userData.employee
   const [editing, setEditing] = useState(false);
   const [newUser, setNewUser] = useState({ ...employeeData });
-  const { email, phone, address, age, specialization, gender, first_name, last_name } = employeeData;
+  const { email, phone, address, age, specialization, gender, first_name, last_name } = newUser;
+  const { apt, street, city, postal, province } = address
 
   const handleUpdate = async() => {
     const updatedUser = await updateEmployee(newUser);
@@ -23,7 +25,18 @@ const UserProfile = () => {
   }
 
   const handleChange = (key, value) => {
-    setNewUser((prevUser) => ({ ...prevUser, [key]: value }));
+    if(key === 'apt' || key ==='street'|| key ==='city' || key ==='postal' || key === 'province'){
+      setNewUser((prevUser) => ({
+        ...prevUser,
+        address: {
+          ...prevUser.address,
+          [key]: value
+        }
+      }))
+    }
+    else{
+      setNewUser((prevUser) => ({ ...prevUser, [key]: value }));
+    }
   };
 
   return (
@@ -54,11 +67,37 @@ const UserProfile = () => {
           </View>
           <View style={styles.inputRow}>
             <Text style={styles.label}>Address: </Text>
-            <TextInput
-              style={styles.input}
-              value={address}
-              onChangeText={(text) => handleChange('address', text)}
-            />
+            <View style={styles.address_section}>
+              <View style={styles.addressInputs}>
+                <TextInput
+                  style={styles.address_apt}
+                  value={apt}
+                  onChangeText={(text) => handleChange('apt', text)}
+                />
+                <TextInput
+                  style={styles.address_street}
+                  value={street}
+                  onChangeText={(text) => handleChange('street', text)}
+                />
+              </View>
+              <View style={styles.addressInputs}>
+                <TextInput
+                  style={styles.address_city}
+                  value={city}
+                  onChangeText={(text) => handleChange('city', text)}
+                />
+                <TextInput
+                  style={styles.address_postal}
+                  value={postal}
+                  onChangeText={(text) => handleChange('postal', text)}
+                />
+              </View>
+              <TextInput
+                style={styles.address_province}
+                value={province}
+                onChangeText={(text) => handleChange('province', text)}
+              />
+            </View>
           </View>
           <View style={styles.inputRow}>
             <Text style={styles.label}>Age:</Text>
@@ -101,7 +140,11 @@ const UserProfile = () => {
         </View>
         {address ? <View style={styles.infoRow}>
           <Text style={styles.infoLabel}>Address:</Text>
-          <Text style={styles.infoValue}>{address}</Text>
+          <View>
+            <Text style={styles.infoValue}>{apt}-{street}</Text>
+            <Text style={styles.infoValue}>{city} {postal}</Text>
+            <Text style={styles.infoValue}>{province}</Text>
+          </View>
         </View> : ''}
         {age ? <View style={styles.infoRow}>
           <Text style={styles.infoLabel}>Age:</Text>
@@ -110,6 +153,7 @@ const UserProfile = () => {
         {specialization ? <View style={styles.infoRow}>
           <Text style={styles.infoLabel}>Specializations:</Text>
           <Text style={styles.infoValue}>{specialization}</Text>
+          
         </View> : ''}
         {gender ? <View style={styles.infoRow}>
           <Text style={styles.infoLabel}>Gender:</Text>
@@ -185,10 +229,53 @@ const styles = StyleSheet.create({
   input: {
     fontSize: 16,
     borderWidth: 1,
-    width: '50%',
+    width: '60%',
     borderRadius: 5,
-    padding: 5
-  }
+    padding: 5,
+  },
+  address_section: {
+    width: '60%',
+  },
+  addressInputs: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    // width: '100%',
+  },
+  address_apt: {
+    width: '15%',
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 5,
+    textAlign: 'center',
+    marginBottom: 10
+  },
+  address_street: {
+    width: '80%',
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 5,
+    marginBottom: 10
+  },
+  address_city: {
+    width: '47.5%',
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 5,
+    marginBottom: 10
+  },
+  address_postal: {
+    width: '47.5%',
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 5,
+    marginBottom: 10
+  },
+  address_province: {
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 5,
+    marginBottom: 10
+  },
 });
 
 export default UserProfile;
